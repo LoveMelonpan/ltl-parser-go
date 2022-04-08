3dfe6e267f08eeb271c3a541ca23984bb9cca737
ppeach <524255034@qq.com>
2022-04-08 10:52:47 +0800

    add norm formula

---
+++ norm.go
@@ -0,0 +1,15 @@
+package ltlparser
+
+type Normalizer struct {
+	formula         string
+	tokenSimplifier TokenSimplifier
+	parser          Parser
+}
+
+func (n *Normalizer) Norm() {
+
+}
+
+func (n *Normalizer) Normalize() {
+
+}
